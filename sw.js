09b23ad25d189b7773698b4640586594f7d92d28
thmforce-cmyk <thmforce@gmail.com>
2026-09-18 20:41:19 +0300

    Add Open Graph preview tags; bump SW cache to jennoise-v3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Jennoise service worker
 // Caches the app on install so it works offline.
 
-const CACHE = 'jennoise-v2';
+const CACHE = 'jennoise-v3';
 const FILES = [
   './',
   './index.html',
